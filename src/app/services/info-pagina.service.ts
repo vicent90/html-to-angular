39f59c0b0ddaf9b9b558cf67de42943fda4f6967
vicentemazzaro@gmail.com <vicentemazzaro@hotmail.com>
2018-11-21 09:49:28 -0300

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
-    this.http.get('/assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
+  private cargarInfo() {
+
+    this.http.get('/assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-8ce9f.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
 
   }
 }
+
